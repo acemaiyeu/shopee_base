d73c53711d3c205f3230349f7c6ebbd687f5902f
Huy FE <nthanhhuy82@gmail.com>
2025-03-28 19:47:37 +0700

    add flash message

--- js/main.js
+++ js/main.js
@@ -22,3 +22,59 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", showFormRegister);
   document.getElementById("login").addEventListener("click", showFormLogin);
 });
+
+// Toasst
+
+toast = ({ title = "", message = "", type = "info", time = 3000 }) => {
+  const main = document.getElementById("toast");
+  const toast = document.createElement("div");
+  toast.classList.add("toast", `toast--${type}`);
+  const icons = {
+    success: "bi bi-check-circle",
+    info: "bi bi-exclamation-circle",
+    warning: "bi bi-exclamation-triangle",
+    error: "bi bi-x-square",
+  };
+  const icon = icons[type];
+  const removeMessage = setTimeout(function () {
+    main.removeChild(toast);
+  }, time);
+
+  toast.onclick = function (e) {
+    if (e.target.closest(".toast__close")) {
+      main.removeChild(toast);
+      clearTimeout(removeMessage);
+    }
+  };
+  toast.innerHTML = `
+          <div class="toast__icon"><i class="${icon}"></i></div>
+          <div class="toast__body">
+            <div class="toast__title">${title}</div>
+            <div class="toast__msg">${message}</div>
+          </div>
+          <i class="toast__close bi bi-dash-circle"></i>`;
+  main.appendChild(toast);
+};
+
+showMessageSuccess = () => {
+  toast({
+    title: "Success!",
+    message: "Đây là thông báo thành công!",
+    type: "success",
+    time: 3000,
+  });
+};
+showMessageError = () => {
+  toast({
+    title: "Error!",
+    message: "Đây là thông báo lỗi!",
+    type: "error",
+    time: 3000,
+  });
+};
+toast({
+  title: "Success!",
+  message: "Đây là trang Website Test. Dựa vào website SHOPEE!",
+  type: "success",
+  time: 3000,
+});
